aa2919c9b369e7ed86646f9e85a146f09bf140e0
Wescley Matos <wescleymatos@gmail.com>
2016-06-20 12:30:11 -0300

    atualização do readme e melhoria do código da entidade User.php a partir da análise do code_sniffer usando PSR2 como padrão

--- README.md
+++ README.md
@@ -1,13 +1,37 @@
 # test-ferramentas-php
 App para realizar testes com ferramentas php
 
-Ferramentas para teste:
- 
+Ferramentas de Gerenciamento de Dependência
+* Composer
+
+Ferramentas para análise de código:
+* PHP Code_Sniffer: Estilo de Código
+* PHPMD: Pontos de Refatoração
+* PHPLoc: Medição e Análise Estrutural
+* PHPCPD: Código Duplicado
+* PHP Depend: Análise e Métrica
+
+Principais comandos:
+* phpcs
+** phpcs --standard=PSR2 path/to/code
+** phpcs --report=xml --report-file=/path/to/file.xml /path/to/code
+
+* phpcpd
+** phpcpd path/to/code
+
+* phploc
+** phploc /path/to/code
+** phploc --log-csv path/to/file.csv --progress /path/to/code
+
+* pdepend
+** pdepend --summary-xml=path/to/file.xml --jdepend-chart=path/to/file.png --overview-pyramid=path/to/file.png path/to/code
+
+Ferramentas para Testes
 * PHPunit
-* PHP Code_Sniffer
-* PHPMD
-* PHPLoc
-* PHPCPD
-* PHP Depend
+* Selenium IDE
+
+Ferramentas de ORM
 * Doctrine
 
+Ferramentas de Injeção de Dependência
+* PHP-DI
--- src/Domain/Entities/User.php
+++ src/Domain/Entities/User.php
@@ -4,48 +4,48 @@ namespace Domain\Entities;
 
 class User
 {
-	private $id;
-	private $name;
-	private $lastName;
+    private $id;
+    private $name;
+    private $lastName;
 
-	function __construct($name, $lastName)
-	{
-		$this->name = $name;
-		$this->lastName = $lastName;
-	}
+    public function __construct($name, $lastName)
+    {
+        $this->name = $name;
+        $this->lastName = $lastName;
+    }
 
-	public function setName($name)
-	{
-		if (empty($name)) {
-			throw new \Exception('The name is not valid.');
-		}
+    public function setName($name)
+    {
+        if (empty($name)) {
+            throw new \Exception('The name is not valid.');
+        }
 
-		$this->name = $name;
-	}
+        $this->name = $name;
+    }
 
-	public function setLastName($lastName)
-	{
-		if (empty($lastName)) {
-			throw new \Exception('The last name is not valid.');
-		}
+    public function setLastName($lastName)
+    {
+        if (empty($lastName)) {
+            throw new \Exception('The last name is not valid.');
+        }
 
-		$this->lastName = $lastName;
-	}
+        $this->lastName = $lastName;
+    }
 
-	public function getFullName()
-	{
-		return sprintf('My name is %s %s', $this->name, $this->lastName);
-	}
+    public function getFullName()
+    {
+        return sprintf('My name is %s %s', $this->name, $this->lastName);
+    }
 
-	public function validate()
-	{
-		if (empty($this->name)) {
-			throw new \Exception('The name is not valid.');
-		}
+    public function validate()
+    {
+        if (empty($this->name)) {
+            throw new \Exception('The name is not valid.');
+        }
 
-		if (empty($this->lastName)) {
-			throw new \Exception('The last name is not valid.');
-		}
-	}
+        if (empty($this->lastName)) {
+            throw new \Exception('The last name is not valid.');
+        }
+    }
 }
 
