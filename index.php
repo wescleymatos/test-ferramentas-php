579316901bb5bcdc9db530ed998140a5c1ac11e5
Wescley Matos <wescleymatos@gmail.com>
2016-06-27 10:12:35 -0300

    correção no path do mapeamento e add classe de validações de domínio

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 require_once "vendor/autoload.php";
 $container = require_once "bootstrap.php";
 
-PATH_ROOT = '/var/www/public/test-doctrine/'
-
 use App\Domain\Services\UserService;
-
+use App\Resource\Validation\AssertionConcern;
 
 try {
 	$userService = $container->get('App\Domain\Services\UserService');
 	$userService->changeName('Carlos', 'Alves');
+
+    AssertionConcern::assertStateFalse(false, 'email inválido!');
 } catch (Exception $e) {
 	echo $e->getMessage();
-}*/
+}
 
