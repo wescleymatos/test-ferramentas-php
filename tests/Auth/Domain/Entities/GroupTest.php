610d0934389c12ff3be53a726161d6f98e63431f
Wescley Matos <wescleymatos@gmail.com>
2016-07-01 09:17:40 -0300

    criação de teste para group->setId passando um parâmetro do tipo errado

--- tests/Auth/Domain/Entities/GroupTest.php
+++ tests/Auth/Domain/Entities/GroupTest.php
@@ -16,19 +16,13 @@ class GroupTest extends \PHPUnit_Framework_TestCase
     public function errorHandler($errno, $errstr, $errfile, $errline)
     {
         throw new \InvalidArgumentException(
-            sprintf(
-                'Missing argument. %s %s %s %s',
-                $errno,
-                $errstr,
-                $errfile,
-                $errline
-            )
+            sprintf('Missing argument. %s %s %s %s', $errno, $errstr, $errfile, $errline)
         );
     }
 
     public function testThrowExceptionWhenInstanceNotPassParamsConstruct()
     {
-        $this->setExpectedException('\InvalidArgumentException');
+        $this->setExpectedException('InvalidArgumentException');
         new Group;
     }
 
@@ -47,24 +41,29 @@ class GroupTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals('gerente', $return);
     }
 
+    public function testGetIdAfterSet()
+    {
+        $this->group->setId(12);
+        $return = $this->group->getId();
+
+        $this->assertEquals(12, $return);
+    }
 
     public function testSetIdWrongTypeHintParam()
     {
-        $this->group->setId(123);
-        $return = $this->group->getId();
-
-        $this->assertEquals(123, $return);
+        $this->setExpectedException('TypeError');
+        $this->group->setId('abc');
     }
 
     public function testThrowExceptionWhenSetNameEmptyParamGiven()
     {
-        $this->setExpectedException('\InvalidArgumentException', 'The name is not valid.');
+        $this->setExpectedException('InvalidArgumentException', 'The name is not valid.');
         $this->group->setName('');
     }
 
     public function testThrowExceptionWhenObjectNotValidation()
     {
-        $this->setExpectedException('\InvalidArgumentException');
+        $this->setExpectedException('InvalidArgumentException');
 
         $group = new Group('');
         $group->validate();
